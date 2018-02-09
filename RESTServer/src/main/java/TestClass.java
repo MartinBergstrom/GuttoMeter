ed1573177c08948cfdcc4f3df36384e0b9970a3a
Martin <emarbe02@gmail.com>
2018-02-09 19:35:31 +0100

    Inital commit

---
+++ RESTServer/src/main/java/TestClass.java
@@ -0,0 +1,21 @@
+/* Copyright (C) 2018 AB Banan - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the AB Banan mem and prenk'd got 'em  license,
+ * which unfortunately won't be written for another century.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of AB Banan Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated
+ * and its suppliers and may be covered by Swärje and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from AB Banan Incorporated.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
