2e2375de64bc6f12a1374190eca9290d68061777
unknown <sunesson.magnus@gmail.com>
2018-09-16 15:04:06 +0200

    hello added methods in DaoConnection-interface and did some dependency interjection for guttstatusresoure.java

--- GuttoMeter/src/com/ab/banan/DaoConnection.java
+++ GuttoMeter/src/com/ab/banan/DaoConnection.java
@@ -6,10 +6,12 @@ public interface DaoConnection {
 	// save to database
 	public GuttStatus saveNewGuttStatus(GuttStatus status);
 	
-	// get from database
+	// get all statuses 
+	public GuttStatus getAll();
 	
 	// get from database with id
+	public GuttStatus getForId(String id);
 	
-	
+	public GuttStatus getAllForId(String id);
 
 }
--- GuttoMeter/src/com/ab/banan/resource/GetGuttStatusResource.java
+++ GuttoMeter/src/com/ab/banan/resource/GetGuttStatusResource.java
@@ -4,14 +4,37 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import com.ab.banan.DaoConnection;
+
+import java.awt.List;
+
+import javax.inject.Inject;
+import javax.websocket.server.PathParam;
+
+
 
 @Path("/")
 public class GetGuttStatusResource {
 	
+	@Inject 
+	private DaoConnection connection;
+			
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON})
-	public String getAll() {
-		return "{ \"all the gutt things\" }";
+	public List getAll() {
+		return getAll();
 	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON})
+	public String getByName(String name) {
+		return getByName(name); 					 
+	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON})
+	public String getAllForName(String name) {
+		return getAllForName(name); 					 
+	}	
 
 }
