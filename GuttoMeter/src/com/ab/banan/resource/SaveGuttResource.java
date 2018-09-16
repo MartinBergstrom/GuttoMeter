28bada7ce8a97edebbf19b15343378ee668cc22b
Martin Bergström <emarbe02@gmail.com>
2018-09-16 15:23:08 +0200

    Wip validation

--- GuttoMeter/src/com/ab/banan/resource/SaveGuttResource.java
+++ GuttoMeter/src/com/ab/banan/resource/SaveGuttResource.java
@@ -1,5 +1,44 @@
 package com.ab.banan.resource;
 
-public class SaveGuttResource {
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.ab.banan.DaoConnection;
+import com.ab.banan.Validation;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+@Path("save")
+public class SaveGuttResource {
+	
+	/*@Inject
+	private DaoConnection connection;
+	@Inject
+	private Validation validation;
+	*/
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response postNewGuttStatus(String body) {
+		JsonParser jsonParser = new JsonParser();
+		JsonElement jsonElement = null;
+		try {
+			jsonElement = jsonParser.parse(body);
+		} catch (JsonSyntaxException jse) {
+			return Response.status(400).entity("{ \"error message\" : \"" + jse.getMessage() + "\" }").build();
+		}
+		
+		
+		//connection.postNewGutt(body)
+		return Response.status(201).build();
+	}
+	
 }
